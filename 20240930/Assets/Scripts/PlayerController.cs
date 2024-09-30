39bdf4a68ae8c7927345a7d2d874167bfbf24399
kdsh627 <105626103+kdsh627@users.noreply.github.com>
2024-09-30 16:37:09 +0900

    20240930 완료

--- 20240930/Assets/Scripts/PlayerController.cs
+++ 20240930/Assets/Scripts/PlayerController.cs
@@ -2,19 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class PlayerController : MonoBehaviour
 {
     public float Speed = 5;
     public float JumpSpeed = 5;
+    public Collider2D BottomCollider;
+    public CompositeCollider2D TerrainCollider; //
 
     float vx = 0;
-
+    bool grounded = true;
+    float prevVx;
+    float prevVy;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        prevVy = GetComponent<Rigidbody2D>().velocity.y;
     }
 
     // Update is called once per frame
@@ -35,12 +40,72 @@ public class PlayerController : MonoBehaviour
         }
 
         //점프
-        if(Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && grounded)
         {
             vy = JumpSpeed;
         }
 
-        GetComponent<Rigidbody2D>().velocity = new Vector2 (vx,vy);
+        //땅과 닿아있을 때
+        if (BottomCollider.IsTouching(TerrainCollider))
+        {
+            //아까는 안붙어 있었음 = 착지
+            if (!grounded)
+            {
+                //vx == 0이면 가만히 있다는 뜻
+                if (vx == 0)
+                {
+                    GetComponent<Animator>().SetTrigger("Idle");
+                }
+                //vx != 0이면 움직이고 있다는 뜻
+                else
+                {
+                    GetComponent<Animator>().SetTrigger("Run");
+                }
+            }
+            //땅에 계속 붙어 있었음
+            else
+            {
+                if (vx != prevVx) //속도가 변할 시에만 1회 작동
+                {
+                    if (vx == 0) //정지
+                    {
+                        GetComponent<Animator>().SetTrigger("Idle");
+                    }
+                    else //달리기
+                    {
+                        GetComponent<Animator>().SetTrigger("Run");
+                    }
+                }
+            }
+        }
+        else
+        {
+            if (grounded) //지금은 안붙어 있는데 아까는 붙어있었음 = 점프
+            {
+                if (vy < 0)
+                {
+                    GetComponent<Animator>().SetTrigger("Fall");
+                }
+                else
+                {   
+                    GetComponent<Animator>().SetTrigger("Jump");
+                }
+            } 
+            //아까도 안붙어 있었음 = 공중에 있음
+            else
+            {
+                if (vy * prevVy < 0)
+                {
+                    GetComponent<Animator>().SetTrigger("Fall");
+                }
+            }
+        }
+
+        grounded = BottomCollider.IsTouching(TerrainCollider); //두 콜라이더의 접촉여부를 반환
+        prevVx = vx;
+        prevVy = vy;
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(vx, vy);
     }
 
     //private void FixedUpdate()
